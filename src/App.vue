d823923b82ae0189fb304f3fa7b31fd7abcb67d0
Fábio Duque <fabio.duque.silva@gmail.com>
2022-10-08 00:37:58 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref, watchEffect } from "vue"
+  
+  const darkMode = ref(document.body.classList.contains('dark'));
+  const applyDarkMode = () => document.body.classList[
+    darkMode.value ? 'add' : 'remove'
+  ]('dark');
+  watchEffect(applyDarkMode);
+    
+</script>
+
+<template>
+  <div>
+    <h1 class="mb-9 p-5 text-5xl font-bold dark:text-slate-50 dark:bg-slate-500 text-slate-900 bg-slate-200">Hello World</h1>
+  </div>
+  <label for="">Activate dark mode</label>
+  <br />
+  <input type="checkbox" name="" id="" v-model="darkMode">
+</template>
+
+<style>
+</style>
